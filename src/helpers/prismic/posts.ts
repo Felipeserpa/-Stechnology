2708e31fc335415d19f2330b96879665b619761c
Samuel Silva <samuel7silva@outlook.com>
2022-10-09 19:07:02 -0300

    create prismic api

---
+++ src/helpers/prismic/posts.ts
@@ -0,0 +1,34 @@
+import {
+  useAllPrismicDocumentsByType,
+  useSinglePrismicDocument,
+} from "@prismicio/react";
+
+// Importa o paragrafo sobre mim do Prismic
+
+export const getAboutMe = () => {
+  const [document] = useSinglePrismicDocument("about_me");
+  if (!document?.data) return null;
+  const data = document?.data;
+  return data;
+};
+
+// Importa os projetos do Prismic
+
+export const getProject = () => {
+  const [document] = useAllPrismicDocumentsByType("portfolio", {
+    orderings: {
+      field: "document.first_publication_date",
+      direction: "desc",
+    },
+  });
+
+  const projects = document?.map((project) => ({
+    slug: project.uid,
+    title: project.data.title[0].text,
+    description: project.data.description[0].text,
+    site: project.data.site.url,
+    github: project.data.github.url,
+    thumbnail: project.data.thumbnail.url,
+  }));
+  return projects;
+};
