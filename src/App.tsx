2f6b1da0d80ece3839e623009a393c902bbef5fb
Samuel Silva <samuel7silva@outlook.com>
2022-10-09 18:56:41 -0300

    import portfolio section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components"
 import { About } from "./assets/components/About";
 import { Footer } from "./assets/components/Footer";
 import { Header } from "./assets/components/Header"
+import { Portfolio } from "./assets/components/Portfolio";
 import { Presentation } from "./assets/components/Presentation";
 import { Skills } from "./assets/components/Skills";
 import { GlobalStyles } from "./assets/styles/GlobalStyles"
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <Presentation />
         <Skills />
+        <Portfolio />
         <About />
         <div className="height"></div>
         <Footer />
