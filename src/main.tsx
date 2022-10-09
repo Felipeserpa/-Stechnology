7c6749bfec64fa39298c7b3763da4d93099e7096
Samuel Silva <samuel7silva@outlook.com>
2022-10-09 18:58:52 -0300

    import prismic provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { PrismicProvider } from "@prismicio/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { client } from "./services/prismic";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PrismicProvider client={client}>
+      <App />
+    </PrismicProvider>
   </React.StrictMode>
-)
+);
