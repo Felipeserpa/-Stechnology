3df3ddb5513da8e727a6b1a767249a14e2a612d1
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-11-11 18:03:34 -0300

    atualizacao de pagina

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { PrismicProvider } from '@prismicio/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import client from './services/prismic';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <PrismicProvider client={client}>
-      <App />
-    </PrismicProvider>
+    <App />
   </React.StrictMode>
 );
