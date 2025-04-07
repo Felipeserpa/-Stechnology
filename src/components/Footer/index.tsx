30bbe3a04bf82832f487622d6f8d5c29b79ba764
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-04-07 12:10:36 -0300

    implementaçao de link para contato

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaEnvelope, FaWhatsapp } from 'react-icons/fa';
 import Container from './styles';
 
 export default function Footer() {
@@ -8,13 +8,33 @@ export default function Footer() {
         <div className="section-container">
           <p>Desenvolvido por @Stechnology</p>
           <div className="social-links">
-            <a href="" target="_blank" rel="noreferrer">
+            <a
+              href=" https://wa.me/5581995773197?text=Olá%2C%20gostaria%20de%20fazer%20um%20orçamento!"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaWhatsapp />
+            </a>
+
+            <a
+              href=" https://github.com/Felipeserpa"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaGithub />
             </a>
-            <a href="l" target="_blank" rel="noreferrer">
+            <a
+              href=" https://www.linkedin.com/in/felipe-serpa-149b88223/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaLinkedin />
             </a>
-            <a href="" target="_blank" rel="noreferrer">
+            <a
+              href="mailto:stecnologico@hotmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
               <FaEnvelope />
             </a>
           </div>
